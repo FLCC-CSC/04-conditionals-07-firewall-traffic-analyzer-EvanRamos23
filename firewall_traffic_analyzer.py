434d6db33bd7bf2eb1c7866bd65a5a0bcf91f1b3
EvanRamos23 <eramos9@fingerlakes.edu>
2026-03-01 17:40:55 +0000

    Wrote a program that considers two pieces of data and makes a risk analysis.

--- firewall_traffic_analyzer.py
+++ firewall_traffic_analyzer.py
@@ -1,8 +1,8 @@
 # FILE NAME - firewall_traffic_analyzer.py
 
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+# NAME: Evan Ramos 
+# DATE: February 28, 2026
+# BRIEF DESCRIPTION:  Write a program that considers two pieces of data and makes a risk analysis. The program monitors data transfers to a network and the risk assessment is based on the port number as well as the size of the transfer.
 
 
 
@@ -14,7 +14,26 @@
 
 
 ########## ENTER YER CODE BELOW THIS LINE ##########
+print("=== Network Traffic Security Analyzer ===")
+print()
 
+port = int(input("Enter the port number (e.g., 80, 22, 443, 3389): "))
+size = int(input("Enter the data transfer size in megabytes (MB): "))
+print()
+
+if (port == 22 or port == 3389) and size >= 100:
+    risk = "HIGH RISK: Potential unauthorized remote access detected!"
+elif port == 80 and size > 100:
+    risk = "MEDIUM RISK: Large unencrypted data transfer detected."
+elif port == 443:
+    risk = "LOW RISK: Secure encrypted transfer detected."
+else:
+    risk = "UNKNOWN: Unrecognized traffic pattern."
+
+print("FIREWALL LOG:")
+print(f"Port: {port}, Transfer Size: {size} MB")
+print(f"Risk Assessment: {risk}")
+print("------------------------")
 
 
 
@@ -89,7 +108,7 @@ Risk Assessment: UNKNOWN: Unrecognized traffic pattern.
 '''
 
 1. Did you get tripped up using the `or` or `and` operators? If so, how?
-
+It was confusing remembering when to use or and when to use and. I also didn’t realize that Python checks and before or so my condition didn’t work the way I thought it would. Once I added parentheses around the or part, it worked correctly.
 
 
 
